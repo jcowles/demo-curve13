83b9e3807749d114ee345c12e614aea3e7a220f7
jcowles <jeremy.cowles@gmail.com>
2013-10-05 10:31:13 -0700

    Added stats factored out main loop to core.js

---
+++ js/core.js
@@ -0,0 +1,46 @@
+var camera, scene, renderer;
+var geometry, material, mesh;
+var stats;
+
+function init() {
+    stats = new Stats();
+    stats.domElement.style.position	= 'absolute';
+    stats.domElement.style.bottom	= '0px';
+    document.body.appendChild( stats.domElement );
+
+    camera = new THREE.PerspectiveCamera( 75, 
+                                window.innerWidth / window.innerHeight, 
+                                1, 10000 );
+    camera.position.z = 1000;
+
+    scene = new THREE.Scene();
+
+    geometry = new THREE.CubeGeometry( 200, 200, 200 );
+    material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
+
+    mesh = new THREE.Mesh( geometry, material );
+    scene.add( mesh );
+
+    renderer = new THREE.WebGLRenderer({
+            antialias               : true,                // to get smoother output
+	    preserveDrawingBuffer   : true     // to allow screenshot
+            });
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+    document.body.appendChild( renderer.domElement );
+
+}
+
+function animate() {
+
+    // note: three.js includes requestAnimationFrame shim
+    requestAnimationFrame( animate );
+
+    mesh.rotation.x += 0.01;
+    mesh.rotation.y += 0.02;
+
+    renderer.render( scene, camera );
+
+    stats.update();
+}
+
