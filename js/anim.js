0905e9d4a2b0a7b9ab3fa8d67afb4e7a3974934e
jcowles <jeremy.cowles@gmail.com>
2013-10-10 21:53:47 -0700

    Added OnBeat class to anim

--- js/anim.js
+++ js/anim.js
@@ -38,3 +38,36 @@ function smoothMap(t0, t1, v0, v1, t) {
 	t = (t - t0) / (t1 - t0);
 	return lerp(smoothStep(t), v0, v1);
 }
+
+// Usage:
+// initialize with 
+//  this.doStuff = new OnBeat([1, 2, 3],
+//        function (time) { log("Hey, it works at t = " + time) },
+//        function (time) { /* turn stuff off */ });
+//
+//  then call
+//  this.doStuff.hit(time)
+//
+//
+OnBeat = function(times, onFunc, offFunc) {
+    this.times = times;
+    this.onFunc = onFunc;
+    this.offFunc = offFunc;
+
+    this.hit = function(time) {
+        var didHit = false;
+        for (var i = 0; i < this.times.length; i++) {
+            if (time >= this.times[i]) {
+                times.splice(i, 1);
+                log(times);
+                this.onFunc(time);
+                didHit = true;
+            }
+        }
+
+        if (didHit)
+            return;
+
+        this.offFunc(time);
+    }
+}
